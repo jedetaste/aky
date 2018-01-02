91caa2cae647387174e3cca4d0e4956e46940c66
Anver Housseini <contact@anver.ch>
2018-01-02 16:41:22 +0100

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-/usr/bin/curl -sfko "/private/tmp/CommandLineTools.sh" "https://raw.githubusercontent.com/jedetaste/aky/master/CommandLineTools.sh"
-/usr/bin/curl -sfko "/private/tmp/Homebrew.sh" "https://raw.githubusercontent.com/jedetaste/aky/master/Homebrew.sh"
+# Install Command Line Tools
 
+/usr/bin/curl -sfko "/private/tmp/CommandLineTools.sh" "https://raw.githubusercontent.com/jedetaste/aky/master/CommandLineTools.sh"
 /bin/chmod +x "/private/tmp/CommandLineTools.sh" && cd "/private/tmp/" && ./CommandLineTools.sh
+
+# Install Homebrew
+
+/usr/bin/curl -sfko "/private/tmp/Homebrew.sh" "https://raw.githubusercontent.com/jedetaste/aky/master/Homebrew.sh"
 /bin/chmod +x "/private/tmp/Homebrew.sh" && cd "/private/tmp/" && ./Homebrew.sh
+
+# Install aky CLI
 
 /usr/bin/curl -sfko "/usr/local/bin/aky" "https://raw.githubusercontent.com/jedetaste/aky/master/bin/aky" && /bin/chmod +x "/usr/local/bin/aky"
